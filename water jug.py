8ca20a8891f835bb020b624292fc781417ab2140
Darshan M R <143525866+darshanmr-911@users.noreply.github.com>
2026-06-05 20:51:30 +0530

    Add files via upload

---
+++ water jug.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+jug1, jug2, aim = 4, 3, 2
+visited = defaultdict(lambda: False)
+
+
+def waterjug(amt1, amt2):
+
+    if (amt1 == aim and amt2 == 0) or (amt2 == aim and amt1 == 0):
+        print(amt1, amt2)
+        return True
+
+    if visited[(amt1, amt2)]:
+        return False
+    print(amt1, amt2)
+    visited[(amt1, amt2)] = True
+
+    return (
+        waterjug(0, amt2)
+        or waterjug(amt1, 0)
+        or waterjug(jug1, amt2)
+        or waterjug(amt1, jug2)
+        or waterjug(amt1 - min(amt1, jug2 - amt2), amt2 + min(amt1, jug2 - amt2))
+        or waterjug(amt1 + min(amt2, jug1 - amt1), amt2 - min(amt2, jug1 - amt1))
+    )
+
+if __name__ == "__main__":
+
+    print("Steps:")
+    if not waterjug(0, 0):
+        print("No solution")
