daa9c8be3be62ad7ba148168552bd0d5890d6938
Darshan M R <143525866+darshanmr-911@users.noreply.github.com>
2026-08-02 21:10:30 +0530

    Add files via upload

---
+++ dictionary compreension.py
@@ -0,0 +1,13 @@
+print("List input practice")
+
+x = [input("Enter the number: ").split()]
+print(x)
+
+
+
+print("List input practice")
+
+x = input("Enter the number: ").split()
+l = [int(num) for num in x]
+
+print(l)
